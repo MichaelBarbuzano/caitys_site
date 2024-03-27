cc89abaa4c0754eb0a6974760bc32b8155282165
MichaelBarbuzano <Michael.Barbuzano@snhu.edu>
2024-03-26 21:55:58 -0400

    Update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import React from 'react';
 import NavBar from './components/navbar'; // Import the NavBar component
 import Gallery from './components/gallery'; // Import the Gallery component
+import './globals.css'
 
 export default function Home() {
   return (
