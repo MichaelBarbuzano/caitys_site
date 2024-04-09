d6d556ed0bd95f1a69b24aace51e166caacf70b1
MichaelBarbuzano <Michael.Barbuzano@snhu.edu>
2024-04-09 16:00:07 -0400

    Updated Image Optimization.

--- next.config.js
+++ next.config.js
@@ -4,5 +4,7 @@ const withOptimizedImages = require('next-optimized-images');
 
 module.exports = withOptimizedImages({
     optimizeImages: true, // Enable image optimization
+    handleImages: ['jpeg', 'png'], // Specify which image types to handle
     /* Other configuration options here */
+    
   });
