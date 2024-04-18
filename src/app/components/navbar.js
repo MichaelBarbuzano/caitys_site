1a24ecdfa015fc144fab813bc6417847ed06a6e1
MichaelBarbuzano <Michael.Barbuzano@snhu.edu>
2024-04-18 15:21:57 -0400

    Update

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -6,11 +6,10 @@ function NavBar() {
     <nav className="nav-bar p-4">
       <div className="home">
         {/* Using the anchor element with background image */}
-        <a href="/" className="home-link"><img src="/assets/Primcon2.png" alt="Description of your image"></img></a>
+        <a href="/" className="home-link"><img src="/assets/Primcon4.png" alt="Description of your image"></img></a>
         
       </div>
       <div className="container mx-auto flex justify-center" style={{ fontSize: '25px', fontFamily: 'impact, sans-serif', textShadow: '0 0 3px pink' }}>
-        <a href="/about" className="text-white ml-4" >About</a>
         <a href="/services" className="text-white ml-4">Services</a>
         <a href="/characters" className="text-white ml-4">Characters</a>
         <a href="/webtoon" className="text-white ml-4">Webtoon</a>
