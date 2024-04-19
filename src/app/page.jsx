9fadb85a47b790ad5aa30f67b247d8c793cb1cc3
MichaelBarbuzano <Michael.Barbuzano@snhu.edu>
2024-04-19 18:57:53 -0400

    Update homepage for mobile

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 
 import Image from 'next/image'
 import React from 'react';
+import Head from 'next/head'; // Import Head from next/head
 import NavBar from './components/navbar'; // Import the NavBar component
 import Gallery from './components/gallery'; // Import the Gallery component
 import './globals.css'
@@ -8,12 +9,14 @@ import './globals.css'
 export default function Home() {
   return (
     <div>
-      {/*Navigation bar */}
-      <NavBar /> 
+      {/* Include the viewport meta tag */}
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      {/* Navigation bar */}
+      <NavBar />
       {/* Include the Gallery component */}
-      
-      <Gallery /> 
-
+      <Gallery />
     </div>
   );
 }
